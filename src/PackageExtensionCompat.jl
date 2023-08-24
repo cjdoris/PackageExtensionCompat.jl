6b903bfe2f8759fef431062e8c5ab1e5b0b6a21e
David Hanak <dhanak@gmail.com>
2023-08-24 12:37:46 +0200

    Use on-the-fly rewrite (no more file writes)

--- src/PackageExtensionCompat.jl
+++ src/PackageExtensionCompat.jl
@@ -5,39 +5,27 @@ export @require_extensions
 const HAS_NATIVE_EXTENSIONS = isdefined(Base, :get_extension)
 
 @static if !HAS_NATIVE_EXTENSIONS
-    using Requires, TOML
+    using MacroTools, Requires, TOML
 
-    function rewrite_import(str, pkgs)
-        parts = split(strip(str))
-        if length(parts) == 1 || (length(parts) ≥ 2 && parts[2] == "as")
-            if parts[1] ∈ pkgs
-                parts[1] = string("..", parts[1])
-            end
+    rewrite(pkgs) = Base.Fix2(rewrite, pkgs)
+
+    function rewrite(expr, pkgs)
+        MacroTools.postwalk(Base.Fix2(rewrite_block, pkgs), expr)
+    end
+
+    function rewrite_block(block, pkgs)
+        !Meta.isexpr(block, [:using, :import]) && return block
+        imports = map(block.args) do use
+            Meta.isexpr(use, [:(:), :as]) ?
+                Expr(use.head, rewrite_use(use.args[1], pkgs), use.args[2:end]...) :
+                rewrite_use(use, pkgs)
         end
-        join(parts, " ")
+        Expr(block.head, imports...)
     end
 
-    function rewrite_imports(str, pkgs)
-        parts = split(str, ",")
-        parts = map(part -> rewrite_import(part, pkgs), parts)
-        join(parts, ", ")
-    end
-
-    function rewrite_line(line, pkgs)
-        pat = r"^(\s*(using|import)\s+)([^;:#$]*[^;:#$\s])(.*)$"
-        m = match(pat, line)
-        if m === nothing
-            line
-        else
-            string(m.captures[1], rewrite_imports(m.captures[3], pkgs), m.captures[4])
-        end
-    end
-
-    function rewrite(srcfile, trgfile, pkgs)
-        lines = readlines(srcfile)
-        lines = map(line -> rewrite_line(line, pkgs), lines)
-        code = join(lines, "\n")
-        write(trgfile, code)
+    function rewrite_use(use::Expr, pkgs)::Expr
+        @assert Meta.isexpr(use, :.)
+        string(use.args[1]) ∈ pkgs ? Expr(:., :., :., use.args...) : use
     end
 
     macro require_extensions()
@@ -56,26 +44,21 @@ const HAS_NATIVE_EXTENSIONS = isdefined(Base, :get_extension)
         extensions = get(toml, "extensions", [])
         isempty(extensions) && error("no extensions defined in $tomlpath")
         exprs = []
-        rm(joinpath(rootdir, "ext_compat"), force=true, recursive=true)
         for (name, pkgs) in extensions
             if pkgs isa String
                 pkgs = [pkgs]
             end
             extpath = nothing
-            for path in [joinpath(rootdir, "ext", "$name.jl"), joinpath(rootdir, "ext", "$name", "$name.jl")]
+            for path in [joinpath(rootdir, "ext", "$name.jl"),
+                         joinpath(rootdir, "ext", "$name", "$name.jl")]
                 if isfile(path)
                     extpath = path
                 end
             end
             extpath === nothing && error("Expecting ext/$name.jl or ext/$name/$name.jl in $rootdir for extension $name.")
-            # rewrite the extension code
-            # TODO: there may be other files to copy/rewrite
             __module__.include_dependency(extpath)
-            extpath2 = joinpath(rootdir, "ext_compat", relpath(extpath, joinpath(rootdir, "ext")))
-            mkpath(dirname(extpath2))
-            rewrite(extpath, extpath2, pkgs)
-            # include the extension code
-            expr = :($(__module__.include)($extpath2))
+            # include and rewrite the extension code
+            expr = :($(__module__.include)($(rewrite(pkgs)), $extpath))
             for pkg in pkgs
                 uuid = get(get(Dict, toml, "weakdeps"), pkg, nothing)
                 uuid === nothing && error("Expecting a weakdep for $pkg in $tomlpath.")
